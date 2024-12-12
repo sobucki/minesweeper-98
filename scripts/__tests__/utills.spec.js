a33dd6951653fd7d8d075f7864bfeee4a699216e
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-12-12 17:08:55 -0300

    Create function to put values of proximity

--- scripts/__tests__/utills.spec.js
+++ scripts/__tests__/utills.spec.js
@@ -1,4 +1,4 @@
-import { createRandomGridMap } from "../utils";
+import { addNumbersToGrid, createRandomGridMap } from "../utils";
 
 describe("utils tests", () => {
   describe("createRandomGridMap", () => {
@@ -36,6 +36,258 @@ describe("utils tests", () => {
       expect(() => {
         createRandomGridMap(2, 0, 1);
       }).toThrow("Invalid rows or cols");
+    });
+  });
+
+  describe("addValuesToMines", () => {
+    it("should add correct value to proximity mines to 2x2", () => {
+      const grid1 = [
+        ["*", 0],
+        [0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid1)).toEqual([
+        ["*", 1],
+        [1, 1],
+      ]);
+
+      const grid2 = [
+        ["*", "*"],
+        [0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid2)).toEqual([
+        ["*", "*"],
+        [2, 2],
+      ]);
+
+      const grid3 = [
+        ["*", "*"],
+        ["*", 0],
+      ];
+
+      expect(addNumbersToGrid(grid3)).toEqual([
+        ["*", "*"],
+        ["*", 3],
+      ]);
+    });
+
+    it("should add correct value to proximity mines to 3x3", () => {
+      const grid1 = [
+        ["*", 0, 0],
+        [0, 0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid1)).toEqual([
+        ["*", 1, 0],
+        [1, 1, 0],
+      ]);
+
+      const grid2 = [
+        ["*", 0, 0],
+        [0, "*", 0],
+      ];
+
+      expect(addNumbersToGrid(grid2)).toEqual([
+        ["*", 2, 1],
+        [2, "*", 1],
+      ]);
+
+      const grid3 = [
+        ["*", "*", 0],
+        [0, "*", 0],
+      ];
+
+      expect(addNumbersToGrid(grid3)).toEqual([
+        ["*", "*", 2],
+        [3, "*", 2],
+      ]);
+
+      const grid4 = [
+        ["*", "*", 0],
+        ["*", "*", 0],
+      ];
+
+      expect(addNumbersToGrid(grid4)).toEqual([
+        ["*", "*", 2],
+        ["*", "*", 2],
+      ]);
+
+      const grid5 = [
+        ["*", 0, "*"],
+        ["*", 0, "*"],
+      ];
+
+      expect(addNumbersToGrid(grid5)).toEqual([
+        ["*", 4, "*"],
+        ["*", 4, "*"],
+      ]);
+
+      const grid6 = [
+        ["*", "*", "*"],
+        ["*", 0, "*"],
+      ];
+
+      expect(addNumbersToGrid(grid6)).toEqual([
+        ["*", "*", "*"],
+        ["*", 5, "*"],
+      ]);
+    });
+
+    it("should add correct value to proximity mines to 4x4", () => {
+      const grid1 = [
+        ["*", 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid1)).toEqual([
+        ["*", 1, 0, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ]);
+
+      const grid2 = [
+        [0, 0, 0, 0],
+        [0, "*", 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid2)).toEqual([
+        [1, 1, 1, 0],
+        [1, "*", 1, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+      ]);
+
+      const grid3 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid3)).toEqual([
+        [1, 2, 2, 1],
+        [1, "*", "*", 1],
+        [1, 2, 2, 1],
+        [0, 0, 0, 0],
+      ]);
+
+      const grid4 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", 0],
+        [0, "*", 0, 0],
+        [0, 0, 0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid4)).toEqual([
+        [1, 2, 2, 1],
+        [2, "*", "*", 1],
+        [2, "*", 3, 1],
+        [1, 1, 1, 0],
+      ]);
+
+      const grid5 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", 0],
+        [0, "*", 0, 0],
+        [0, "*", 0, 0],
+      ];
+
+      expect(addNumbersToGrid(grid5)).toEqual([
+        [1, 2, 2, 1],
+        [2, "*", "*", 1],
+        [3, "*", 4, 1],
+        [2, "*", 2, 0],
+      ]);
+
+      const grid6 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", 0],
+        [0, "*", 0, 0],
+        [0, "*", "*", 0],
+      ];
+
+      expect(addNumbersToGrid(grid6)).toEqual([
+        [1, 2, 2, 1],
+        [2, "*", "*", 1],
+        [3, "*", 5, 2],
+        [2, "*", "*", 1],
+      ]);
+
+      const grid7 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", 0],
+        [0, "*", 0, 0],
+        [0, "*", "*", "*"],
+      ];
+
+      expect(addNumbersToGrid(grid7)).toEqual([
+        [1, 2, 2, 1],
+        [2, "*", "*", 1],
+        [3, "*", 6, 3],
+        [2, "*", "*", "*"],
+      ]);
+
+      const grid8 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", 0],
+        [0, "*", 0, "*"],
+        [0, "*", "*", "*"],
+      ];
+
+      expect(addNumbersToGrid(grid8)).toEqual([
+        [1, 2, 2, 1],
+        [2, "*", "*", 2],
+        [3, "*", 7, "*"],
+        [2, "*", "*", "*"],
+      ]);
+
+      const grid9 = [
+        [0, 0, 0, 0],
+        [0, "*", "*", "*"],
+        [0, "*", 0, "*"],
+        [0, "*", "*", "*"],
+      ];
+
+      expect(addNumbersToGrid(grid9)).toEqual([
+        [1, 2, 3, 2],
+        [2, "*", "*", "*"],
+        [3, "*", 8, "*"],
+        [2, "*", "*", "*"],
+      ]);
+
+      const grid10 = [
+        ["*", 0, 0, 0],
+        [0, "*", 0, 0],
+        [0, 0, "*", 0],
+        [0, 0, 0, "*"],
+      ];
+
+      expect(addNumbersToGrid(grid10)).toEqual([
+        ["*", 2, 1, 0],
+        [2, "*", 2, 1],
+        [1, 2, "*", 2],
+        [0, 1, 2, "*"],
+      ]);
+
+      const grid11 = [
+        ["*", 0, "*", 0],
+        [0, "*", 0, "*"],
+        ["*", 0, "*", 0],
+        [0, "*", 0, "*"],
+      ];
+
+      expect(addNumbersToGrid(grid11)).toEqual([
+        ["*", 3, "*", 2],
+        [3, "*", 4, "*"],
+        ["*", 4, "*", 3],
+        [2, "*", 3, "*"],
+      ]);
     });
   });
 });
--- scripts/utils.js
+++ scripts/utils.js
@@ -20,3 +20,25 @@ export function createRandomGridMap(rows, cols, mines) {
   }
   return grid;
 }
+
+export function addNumbersToGrid(grid) {
+  let newGrid = grid.map((row, rowIndex) => {
+    return row.map((cell, colIndex) => {
+      if (cell === "*") {
+        return cell;
+      }
+      let mines = 0;
+      for (let i = rowIndex - 1; i <= rowIndex + 1; i++) {
+        for (let j = colIndex - 1; j <= colIndex + 1; j++) {
+          if (i >= 0 && j >= 0 && i < grid.length && j < row.length) {
+            if (grid[i][j] === "*") {
+              mines++;
+            }
+          }
+        }
+      }
+      return mines;
+    });
+  });
+  return newGrid;
+}
